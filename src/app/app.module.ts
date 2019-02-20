67cceb7974669929e5b42ac924524bf8c010e8af
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-19 20:57:39 -0500

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,15 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent, CockpitComponent, ServerElementComponent
+    ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent, CockpitComponent, ServerElementComponent, GameControlComponent, OddComponent, EvenComponent
   ],
   imports: [
     BrowserModule,
