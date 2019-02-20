d74542525ec539b9d1000f7830726728dd655dd4
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-19 20:57:01 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,37 @@ export class AppComponent {
       content:'test server content'}
   ];
 
+  onServerAdded(serverData: {serverName: string, serverContent:string}) {
+    
+    this.serverElements.push({
+      type:'server',
+      name:serverData.serverName,
+      content:serverData.serverContent
+    })
+    
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent:string}) {
+    
+    this.serverElements.push({
+      type:'blueprint',
+      name:blueprintData.serverName,
+      content:blueprintData.serverContent
+    })
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed !'
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0,1)
+  }
+
+  onGameStarted(gameData: {numberEvent:string}) {
+    console.log(gameData)
+  }
+  onGameStopped(gameData:{numberEvent:string}) {
+    console.log(gameData)
+  }
 }
