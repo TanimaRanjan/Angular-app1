38bbac8ef1672352a6865f483a16a5e56c7bf892
Tanima Ranjan <tanimaranjan@gmail.com>
2019-03-13 22:06:27 -0400

    updates

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,4 +1,8 @@
-export class AccountService 
+import { LoggingService } from './logging.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AccountsService 
 {
     accounts = [
         {
@@ -15,11 +19,15 @@ export class AccountService
         }
       ];
 
+      constructor(private loggingService: LoggingService) {}
+
       addAccount(name:string, status: string) {
         this.accounts.push({name:name, status:status})
+        this.loggingService.logStatusChange(status)
       }
 
       updateStatus(id:number, status:string) {
         this.accounts[id].status = status
+        this.loggingService.logStatusChange(status)
       }
 }
