a406855bb5cafa84a273d232fb612e08788472b3
Tanima Ranjan <tanimaranjan@gmail.com>
2019-03-20 22:13:18 -0400

    server components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 // import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+ import { AppComponent } from './app.component';
+//  import { ServerComponent } from './server/server.component';
+//  import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
@@ -20,7 +20,16 @@ import { NewAccountComponent } from './new-account/new-account.component';
 import { AccountComponent } from './account/account.component'
 
 import { AccountsService }  from './accounts.service'
-import { LoggingService } from './logging.service';
+import { LoggingService } from './logging.service'
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
@@ -38,7 +47,7 @@ import { LoggingService } from './logging.service';
     BetterHighlightDirective, 
     UnlessDirective, 
     NewAccountComponent, 
-    AccountComponent
+    AccountComponent, EditServerComponent
   ],
   imports: [
     BrowserModule,
