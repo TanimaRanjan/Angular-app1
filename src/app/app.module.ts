5c37223e7f02396856aafe4afb960fbf2c95b3bd
Tanima Ranjan <tanimaranjan@gmail.com>
2019-03-23 17:41:41 -0400

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router'
 // import { HttpModule } from '@angular/http';
 
 //  import { AppComponent } from './app.component';
@@ -31,6 +32,12 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
+const appRoutes: Routes = [
+  { path: '' , component:  HomeComponent } ,
+  { path: 'users' , component:  UsersComponent } ,
+  { path: 'servers' , component:  ServersComponent } 
+]
+
 @NgModule({
   declarations: [
     AppComponent, 
@@ -55,7 +62,8 @@ import { ServersService } from './servers/servers.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule, 
+    RouterModule.forRoot(appRoutes)
     // ,
     // HttpModule
   ],
