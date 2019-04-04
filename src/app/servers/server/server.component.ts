2a2603ffbea34421a2165081de886e4c4880d426
Tanima Ranjan <tanimaranjan@gmail.com>
2019-04-03 21:26:22 -0400

    updates

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -7,10 +7,13 @@ import { ServersService } from '../servers.service';
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
+
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService) { 
+    
+  }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
