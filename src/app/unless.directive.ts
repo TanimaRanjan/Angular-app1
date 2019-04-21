f3676ed14f6bea68d8d36bf78d0c6b233aafb675
Tanima Ranjan <tanimaranjan@gmail.com>
2019-04-20 22:32:37 -0400

    update

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -4,6 +4,7 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
   selector: '[appUnless]'
 })
 export class UnlessDirective {
+
   @Input() set appUnless(condition: boolean) {
     if(!condition) {
       this.vcRef.createEmbeddedView(this.templateRef)
@@ -11,7 +12,7 @@ export class UnlessDirective {
       this.vcRef.clear()
     }
   }
-
-  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
-
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { 
+    
+  }
 }
